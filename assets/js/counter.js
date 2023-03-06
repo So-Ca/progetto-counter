420299cc5c7e6ca34a1fed08f4307f749d57c85a
So-Ca <sonnycapu@gmail.com>
2023-03-06 13:06:57 +0100

    added correction

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,40 +1,36 @@
 let container = document.querySelector(".container")
 let containerTwo = document.querySelector(".container-two")
 
-let count = document.createElement("div");
-count.id = "count";
-count.innerHTML = "";
-container.appendChild(count);
+function createElement(tagName , className, innerHTML, parent) {
+  const element = document.createElement(tagName);
+  element.className = className;
+  element.innerHTML = innerHTML;
+  parent.appendChild(element);
+  return element;
+}
 
-let subtract = document.createElement('button');
-subtract.className = "subtract";
-subtract.innerHTML = "-";
-containerTwo.appendChild(subtract);
+const count = createElement("div", "count", "", container);
+const subtract = createElement("button", "subtract", "-", containerTwo);
+const clear = createElement("button", "clear", "Clear", containerTwo);
+const add = createElement("button", "add", "+", containerTwo);
 
-let clear = document.createElement('button');
-clear.className = "clear";
-clear.innerHTML = "Clear";
-containerTwo.appendChild(clear);
-
-let add = document.createElement('button');
-add.className = "add";
-add.innerHTML = "+";
-containerTwo.appendChild(add);
 
 let counter = 0;
 let gifVisible = false;
-const counting = document.getElementById('count');
+
+const counting = document.querySelector('.count');
 const buttons = document.querySelectorAll('button');
 
+const audioMinus = new Audio('assets/audio/bump.mp3');
+const audioPlus = new Audio('assets/audio/coin.mp3');
+const audioClear = new Audio('assets/audio/pause.mp3');
+
+const gifPlus = document.querySelector('.gif-plus');
+const gifMinus = document.querySelector('.gif-minus');
 
 buttons.forEach( function (button) {
   button.addEventListener('click', function(e) {
 
-    const audioMinus = new Audio('assets/audio/bump.mp3');
-    const audioPlus = new Audio('assets/audio/coin.mp3');
-    const audioClear = new Audio('assets/audio/pause.mp3');
-    const gifPlus = document.querySelector('.gif-plus');
-    const gifMinus = document.querySelector('.gif-minus');
     const type = e.currentTarget.classList;
 
     if (type.contains("subtract")) {
@@ -42,25 +38,25 @@ buttons.forEach( function (button) {
       audioMinus.play();
       gifMinus.classList.remove("hidden");
       if (!gifVisible) {
-        gifMinus.style.opacity = "1";
-        gifVisible = true;
-        setTimeout(function(){
-          gifMinus.style.opacity = "0";
-          gifVisible = false;
-        }, 900);
-      }
+       gifMinus.style.opacity = "1";
+       gifVisibile = true;
+       setTimeout(function(){
+         gifMinus.style.opacity = "0";
+         gifVisibile = false;
+       }, 900);
+     }
     } else if (type.contains('add')){
       counter ++;
       audioPlus.play();
       gifPlus.classList.remove("hidden");
       if (!gifVisible) {
-        gifPlus.style.opacity = "1";
-        gifVisible = true;
-        setTimeout(function(){
-          gifPlus.style.opacity = "0";
-          gifVisible = false;
-        }, 900);
-      }
+       gifPlus.style.opacity = "1";
+       gifVisibile = true;
+       setTimeout(function(){
+         gifPlus.style.opacity = "0";
+         gifVisibile = false;
+       }, 900);
+     }
     } else {
       counter = 0;
       audioClear.play();
